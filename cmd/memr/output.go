02c82675f4af0d404beba3e7cdc4da2d24eea0c2
Ryan Deivert <ryandeivert@gmail.com>
2022-02-21 22:29:39 -0800

    update cli / examples to use aws-sdk-go-v2

--- cmd/memr/output.go
+++ cmd/memr/output.go
@@ -1,34 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/golang/snappy"
 )
 
-func S3Writer(reader io.ReadCloser, compress bool, region, bucket, key string, concurrency int) (*s3manager.UploadOutput, error) {
+func S3Writer(reader io.ReadCloser, compress bool, region, bucket, key string, concurrency int) (*manager.UploadOutput, error) {
 
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{Region: aws.String(region)},
-	}))
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		// client will only use this region if none is otherwise set using AWS_REGION or AWS_DEFAULT_REGION
+		config.WithDefaultRegion(region),
+	)
+	if err != nil {
+		return nil, err
+	}
 
 	// Create an uploader with the session and custom options
-	uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader) {
-		u.PartSize = s3manager.MinUploadPartSize // 5MB (smallest allowed)
+	uploader := manager.NewUploader(s3.NewFromConfig(cfg), func(u *manager.Uploader) {
+		u.PartSize = manager.MinUploadPartSize // 5MB (smallest allowed)
 		// 5 goroutines is the default. A lower value here will have a lesser
 		// impact on memory pressure, and should be considered
 		// Increasing this value will directly impact how much memory we use
-		// set to s3manager.DefaultUploadConcurrency by default
+		// set to manager.DefaultUploadConcurrency by default
 		u.Concurrency = concurrency
 
 		// A buffer provider could be used to allow for larger buffers (64 KiB?) in memory
-		// u.BufferProvider = s3manager.NewBufferedReadSeekerWriteToPool(64 * 1024)
+		// u.BufferProvider = manager.NewBufferedReadSeekerWriteToPool(64 * 1024)
 	})
 
 	var s3Reader io.Reader = reader
@@ -62,12 +68,14 @@ func S3Writer(reader io.ReadCloser, compress bool, region, bucket, key string, c
 	}
 
 	// Upload the file to S3
-	result, err := uploader.Upload(&s3manager.UploadInput{
-		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-		Body:   s3Reader,
-	})
+	result, err := uploader.Upload(context.TODO(),
+		&s3.PutObjectInput{
+			ACL:    types.ObjectCannedACLBucketOwnerFullControl,
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+			Body:   s3Reader,
+		},
+	)
 
 	if err != nil {
 		return nil, fmt.Errorf("failed to upload to s3: %s", err)
--- examples/stream_s3/main.go
+++ examples/stream_s3/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"compress/gzip"
 	"compress/zlib"
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/golang/snappy"
 	"github.com/pierrec/lz4/v4"
 	"github.com/ryandeivert/memr"
@@ -22,7 +24,7 @@ type compressorFunc func(io.Writer) io.WriteCloser
 var bucketFlag = flag.String("bucket", "", "S3 bucket to which memory should be streamed")
 var keyFlag = flag.String("key", "", "key for object in S3")
 var compressionFlag = flag.String("compression", "snappy", "type of compression to use for output (snappy, lz4, zlib, gzip, none)")
-var concurrencyFlag = flag.Int("threads", s3manager.DefaultUploadConcurrency, "number of goroutines to use for upload")
+var concurrencyFlag = flag.Int("threads", manager.DefaultUploadConcurrency, "number of goroutines to use for upload")
 
 func main() {
 
@@ -82,22 +84,28 @@ func main() {
 }
 
 // Copy the data to S3 through the open reader, using optional compression
-func copyToS3(reader io.ReadCloser, compressor compressorFunc, bucket, key string) (*s3manager.UploadOutput, error) {
-	sess := session.Must(session.NewSession())
+func copyToS3(reader io.ReadCloser, compressor compressorFunc, bucket, key string) (*manager.UploadOutput, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		// client will only use this region if none is otherwise set using AWS_REGION or AWS_DEFAULT_REGION
+		config.WithDefaultRegion("us-east-1"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	// Create an uploader with the session and custom options
-	uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader) {
-		// PartSize: 5MB (smallest allowed)
-		u.PartSize = s3manager.MinUploadPartSize
+	uploader := manager.NewUploader(s3.NewFromConfig(cfg), func(u *manager.Uploader) {
+		u.PartSize = manager.MinUploadPartSize // 5MB (smallest allowed)
 
 		// Concurrency: 5 goroutines is the default. A lower value here will
 		// have a lesser impact on memory pressure, and should be considered
 		// Increasing this value will directly impact how much memory we use
-		// set to s3manager.DefaultUploadConcurrency by default
+		// set to manager.DefaultUploadConcurrency by default
 		u.Concurrency = *concurrencyFlag
 
 		// BufferProvider: this could be used to allow for larger buffers (64 KiB?)
 		// in memory but may not be desirable if memory pressure is of concern
-		// u.BufferProvider = s3manager.NewBufferedReadSeekerWriteToPool(64 * 1024)
+		// u.BufferProvider = manager.NewBufferedReadSeekerWriteToPool(64 * 1024)
 	})
 
 	var s3Reader io.Reader = reader
@@ -131,12 +139,14 @@ func copyToS3(reader io.ReadCloser, compressor compressorFunc, bucket, key strin
 	}
 
 	// Upload the file to S3
-	result, err := uploader.Upload(&s3manager.UploadInput{
-		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-		Body:   s3Reader,
-	})
+	result, err := uploader.Upload(context.TODO(),
+		&s3.PutObjectInput{
+			ACL:    types.ObjectCannedACLBucketOwnerFullControl,
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+			Body:   s3Reader,
+		},
+	)
 
 	if err != nil {
 		return nil, fmt.Errorf("failed to upload to s3: %s", err)
